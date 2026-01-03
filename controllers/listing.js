fd2c6f22367d4ff1bb652e578e565b6632b25cfd
Amrutha261104 <isikala26@gmail.com>
2026-01-03 19:53:54 +0530

    Fix listing schema validation and defaults

--- controllers/listing.js
+++ controllers/listing.js
@@ -27,17 +27,33 @@ module.exports.showListing = async(req,res) => {
     res.render("listings/show.ejs", {listing});
 };
 
-module.exports.createListing = async(req,res ,next)=>{
-    let url = req.file.path; //for ectype for image uploade instead url upload
-    let filename = req.file.filename;
-    const newlisting=new Listing(req.body.listing);
-    newlisting.owner = req.user._id;
-    newlisting.image = {url,filename};
-   await newlisting.save();
-   req.flash("success", "New LIsting Added");  // sucess is the key and msg to be displayed is new listing added
-   res.redirect("/listings");
+// module.exports.createListing = async(req,res ,next)=>{
+//     let url = req.file.path; //for ectype for image uploade instead url upload
+//     let filename = req.file.filename;
+//     const newlisting=new Listing(req.body.listing);
+//     newlisting.owner = req.user._id;
+//     newlisting.image = {url,filename};
+//    await newlisting.save();
+//    req.flash("success", "New LIsting Added");  // sucess is the key and msg to be displayed is new listing added
+//    res.redirect("/listings");
  
+// };
+module.exports.createListing = async (req, res, next) => {
+    const newlisting = new Listing(req.body.listing);
+    newlisting.owner = req.user._id;
+
+    if (req.file) {
+        newlisting.image = {
+            url: req.file.path,
+            filename: req.file.filename
+        };
+    }
+
+    await newlisting.save();
+    req.flash("success", "New Listing Added");
+    res.redirect("/listings");
 };
+
 
 module.exports.renderEditForm = async(req,res)=>{
     let {id} = req.params;
@@ -46,20 +62,38 @@ module.exports.renderEditForm = async(req,res)=>{
         req.flash("error","listing you requested for doesnot exists");
         res.redirect("/listings");
     }
-     let originalImageUrl = listing.image.url;
-     originalImageUrl.replace("/upload","/upload/w_250");
+    //  let originalImageUrl = listing.image.url;
+    //  originalImageUrl.replace("/upload","/upload/w_250");
+    let originalImageUrl = listing.image.url.replace(
+        "/upload",
+        "/upload/w_250"
+    );
+
     res.render("listings/edit.ejs",{listing,originalImageUrl});
 
 };
 
 module.exports.updateListing =  async(req,res)=>{
     let {id} = req.params;
-    let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    // let listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
 
-    if(typeof ref.file !=="undefined") {
-        let url = req.file.path;
-        let filename = req.file.filename;
-        listing.image = {url,filename};
+    // if(typeof ref.file !=="undefined") {
+    //     let url = req.file.path;
+    //     let filename = req.file.filename;
+    //     listing.image = {url,filename};
+    //     await listing.save();
+    // }
+    let listing = await Listing.findByIdAndUpdate(
+        id,
+        { ...req.body.listing },
+        { new: true }
+    );
+
+    if (req.file) {
+        listing.image = {
+            url: req.file.path,
+            filename: req.file.filename
+        };
         await listing.save();
     }
      req.flash("success", "a listing updated"); 
--- models/listing.js
+++ models/listing.js
@@ -1,19 +1,69 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+// const review = require("./review.js");
+
+// const listingSchema = new Schema({
+//     title: {
+//         type: String,
+//         requied: true,
+//     },
+//     description: String,
+//     image: {
+//         url: String,
+//         filename: String,
+        
+//     },
+//     price: Number,
+//     location: String,
+//     country: String,
+//     reviews: [
+//         {
+//             type: Schema.Types.ObjectId,
+//             ref: "Review",
+//         },
+//     ],
+//     owner: {
+//         type: Schema.Types.ObjectId,
+//         ref: "User",
+//     },
+    
+// });
+
+// listingSchema.post("findOneAndDelete",async (listing) =>{
+//     if(listing) {
+//         await review.deleteMany({ _id: { $in: listing.reviews} });
+//     }
+// });
+
+// const Listing= mongoose.model("Listing", listingSchema);
+// module.exports = Listing;
+
+
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const review = require("./review.js");
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title: {
         type: String,
-        requied: true,
+        required: true,
     },
     description: String,
     image: {
-        url: String,
-        filename: String,
-        
+        url: {
+            type: String,
+            default: "https://res.cloudinary.com/demo/image/upload/sample.jpg",
+        },
+        filename: {
+            type: String,
+            default: "default",
+        },
     },
-    price: Number,
+    price: {
+        type: Number,
+        min: 0,
+    },
     location: String,
     country: String,
     reviews: [
@@ -26,18 +76,12 @@ const listingSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "User",
     },
-    // category: {
-    //     type: String,
-    //     enum: ["moutains","arctic","farms","deserts"]
-    // }
-    
 });
 
-listingSchema.post("findOneAndDelete",async (listing) =>{
-    if(listing) {
-        await review.deleteMany({ _id: { $in: listing.reviews} });
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
     }
 });
 
-const Listing= mongoose.model("Listing", listingSchema);
-module.exports = Listing;
+module.exports = mongoose.model("Listing", listingSchema);
